ca4f9726185b45a0a8d74a76a85ce84ad9ea1e44
aurelienv <aurelienvpro@gmail.com>
2024-10-02 15:12:49 +0200

    feat: implement sms verification

--- src/pages/introduction/Introduction.tsx
+++ src/pages/introduction/Introduction.tsx
@@ -1,18 +1,21 @@
 import assets from "@assets/index";
 import MyScreen from "@src/components/MyScreen";
+import MyButton from "@src/components/natives/MyButton";
 import MyImage from "@src/components/natives/MyImage";
 import MyText from "@src/components/natives/MyText";
+import { checkCode, sendSMS } from "@src/utils/phone";
 import React from "react";
 import { View } from "react-native";
 
-const Introduction = ({ navigation }: { navigation: any }) => {
-  console.log("Introduction");
+const Introduction = () => {
   return (
     <MyScreen className="justify-center">
       <View>
         <MyImage img={assets.icon} containerStyle="h-80" />
         <MyText className="text-2xl font-semibold mb-5">poolp</MyText>
       </View>
+      <MyButton onPress={sendSMS} txt="Envoyer un SMS" />
+      <MyButton onPress={checkCode} txt="Vérifier le code" />
       <MyText className={"text-2xl font-semibold mb-5"}>
         Introduction page
       </MyText>
