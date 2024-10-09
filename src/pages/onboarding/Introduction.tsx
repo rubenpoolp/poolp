2b9225e45cf27986f18897de9fbec1eae9e8d28e
aurelienv <aurelienvpro@gmail.com>
2024-10-09 11:38:22 +0200

    chore: remove test in introduction screen

--- src/pages/onboarding/Introduction.tsx
+++ src/pages/onboarding/Introduction.tsx
@@ -1,11 +1,7 @@
-import MyTextInput from "@components/inputs/MyTextInput";
 import LogoWithText from "@components/LogoWithText";
-import MyButton from "@components/natives/MyButton";
 import MyText from "@components/natives/MyText";
-import { useAuth } from "@context/Auth";
 import MyOnboardingLayout from "@pages/onboarding/MyOnboardingLayout";
-import resetTo from "@utils/resetTo";
-import React, { useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 
@@ -17,12 +13,6 @@ const Introduction = ({
   route: any;
 }) => {
   const { t } = useTranslation();
-  const [code, setCode] = useState("");
-  const [phone, setPhone] = useState(
-    process.env.EXPO_PUBLIC_TEST_PHONE_NUMBER ?? "",
-  );
-  const { sendSMS, checkCode, signOut } = useAuth();
-  const hide = true;
   const { user, nextScreen } = route.params;
 
   const handleNext = () => {
@@ -56,33 +46,6 @@ const Introduction = ({
         <MyText className={"text-center"}>
           {t("introduction.description5")}
         </MyText>
-      </View>
-      <View className={`absolute right-0 top-1/2 w-full ${hide && "hidden"}`}>
-        <MyTextInput
-          className="w-full bg-light px-4 py-3 mt-2 mb-2 text-black"
-          placeholder="Phone"
-          value={phone}
-          onChangeText={setPhone}
-          onSubmitEditing={() => sendSMS(phone)}
-        />
-        <MyButton onPress={() => sendSMS(phone)} txt={`SEND SMS TO ${phone}`} />
-        <MyTextInput
-          className="w-full bg-light px-4 py-3 mt-2 mb-2 text-black"
-          placeholder="Code"
-          value={code}
-          onChangeText={setCode}
-          onSubmitEditing={() =>
-            checkCode(phone, code).then(() => resetTo(navigation, "HomeStack"))
-          }
-        />
-        <MyButton
-          className="mb-2"
-          onPress={() =>
-            checkCode(phone, code).then(() => resetTo(navigation, "HomeStack"))
-          }
-          txt={"Check code"}
-        />
-        <MyButton onPress={() => signOut()} txt={"Sign out"} />
       </View>
     </MyOnboardingLayout>
   );
