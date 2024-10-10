fb711966913804dd1dc9c26c1a38430feaaad197
Enzo Ventura <enzo@trackap.com>
2024-10-10 21:41:46 +0200

    fix: split item & list

---
+++ src/components/SchoolListItem.tsx
@@ -0,0 +1,43 @@
+import { View } from "react-native";
+import { Bump } from "@components/animations/Bump";
+import MyButton from "@components/natives/MyButton";
+import MyText from "@components/natives/MyText";
+import { Schools } from "@components/modals/SchoolListModal";
+
+const SchoolItem = ({
+  school,
+  onSelect,
+}: {
+  school: (typeof Schools)[0]; // TODO: change type when api is ready
+  onSelect: (_school: any) => void;
+}) => {
+  return (
+    <View className="flex-row items-center border-b border-gray-400 py-4 px-4 space-x-4 justify-between">
+      <View className="flex-row items-center space-x-4">
+        <View className="border border-light rounded-full pt-1.5 pb-3 px-3 ">
+          <MyText className="font-semibold text-2xl">🏫</MyText>
+        </View>
+
+        <View>
+          <MyText className="font-semibold">{school.name}</MyText>
+          <MyText className="text-gray-500 font-light">
+            {school.city}, {school.country}
+          </MyText>
+        </View>
+      </View>
+
+      <Bump scaleValue={0.95}>
+        <MyButton
+          txt="Select"
+          onPress={() => {
+            onSelect(school);
+          }}
+          className=""
+          size="small"
+        />
+      </Bump>
+    </View>
+  );
+};
+
+export default SchoolItem;
--- src/components/modals/SchoolListModal.tsx
+++ src/components/modals/SchoolListModal.tsx
@@ -12,8 +12,9 @@ import { Bump } from "@components/animations/Bump";
 import MyButton from "@components/natives/MyButton";
 import { GraduationCap } from "phosphor-react-native";
 import MySearchInput from "@components/inputs/MySearchInput";
+import SchoolItem from "@components/SchoolListItem";
 
-const Schools = [
+export const Schools = [
   {
     name: "Stanford University",
     city: "Stanford",
@@ -69,42 +70,6 @@ const Schools = [
     id: "9",
   },
 ];
-
-const SchoolItem = ({
-  school,
-  onSelect,
-}: {
-  school: (typeof Schools)[0];
-  onSelect: (_school: any) => void;
-}) => {
-  return (
-    <View className="flex-row items-center border-b border-gray-400 py-4 px-4 space-x-4 justify-between">
-      <View className="flex-row items-center space-x-4">
-        <View className="border border-light rounded-full pt-1.5 pb-3 px-3 ">
-          <MyText className="font-semibold text-2xl">🏫</MyText>
-        </View>
-
-        <View>
-          <MyText className="font-semibold">{school.name}</MyText>
-          <MyText className="text-gray-500 font-light">
-            {school.city}, {school.country}
-          </MyText>
-        </View>
-      </View>
-
-      <Bump scaleValue={0.95}>
-        <MyButton
-          txt="Select"
-          onPress={() => {
-            onSelect(school);
-          }}
-          className=""
-          size="small"
-        />
-      </Bump>
-    </View>
-  );
-};
 
 const EmptyList = () => {
   return (
