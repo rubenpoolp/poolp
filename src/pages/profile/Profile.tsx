c37e17b591c07239ed067f17dd6c7c02723b4dcc
Enzo Ventura <enzo@trackap.com>
2024-10-10 16:38:59 +0200

    feat: profile

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -10,17 +10,33 @@ import { useNavigation } from "@react-navigation/native";
 import resetTo from "@utils/resetTo";
 import React, { FC } from "react";
 import { Alert, Linking, ScrollView, View } from "react-native";
+import { useTranslation } from "react-i18next";
+import MyButton from "@components/natives/MyButton";
+import { Bump } from "@components/animations/Bump";
 
-const DisplayInfo = ({ title, value }: { title: string; value: string }) => {
+const DisplayInfo = ({
+  title,
+  value,
+  txtClassName,
+}: {
+  title: string;
+  value: string;
+  txtClassName?: string;
+}) => {
+  const { t } = useTranslation();
+
   return (
-    <View className="w-full flex-row justify-between items-center mb-2">
-      <MyText className="text-lg font-semibold">{title}</MyText>
-      <MyText className="text-lg">{value}</MyText>
+    <View className="w-full flex-row justify-between items-center mb-6">
+      <MyText className={`text-lg font-semibold ${txtClassName}`}>
+        {t(title)}
+      </MyText>
+      <MyText className="text-gray-500 font-semibold">{value}</MyText>
     </View>
   );
 };
 
 const Profile = () => {
+  const { t } = useTranslation();
   const navigation = useNavigation();
   const { isAdmin, signOut } = useAuth();
 
@@ -35,28 +51,49 @@ const Profile = () => {
     name: string;
     onPress: () => void;
     icon?: FC;
+    value?: string;
+    txtClassName?: string;
   }[] = [
     {
-      name: "Contact",
+      name: "profile.name",
       onPress: () => {
         Linking.openURL("mailto:ruben@gmail.com"); // TODO: change to the right email
       },
+      value: "Lola",
     },
 
     {
-      name: "Conditions Générales d’Utilisation",
+      name: "profile.school",
+      onPress: () => Linking.openURL(""),
+      value: "Sebeweiss High School",
+    },
+    {
+      name: "profile.invite",
       onPress: () => Linking.openURL(""),
     },
     {
-      name: "Données personnelles",
+      name: "profile.pushNotifications",
       onPress: () => Linking.openURL(""),
     },
     {
-      name: "Se déconnecter",
+      name: "profile.help",
+      onPress: () => Linking.openURL(""),
+    },
+    {
+      name: "profile.rate",
+      onPress: () => Linking.openURL(""),
+    },
+    {
+      name: "profile.aboutUs",
+      onPress: () => Linking.openURL(""),
+    },
+
+    {
+      name: "profile.logout.title",
       onPress: () => {
-        Alert.alert("Déconnexion", "Es-tu sûr de vouloir te déconnecter ?", [
+        Alert.alert(t("profile.logout.title"), t("profile.logout.areYouSure"), [
           {
-            text: "Annuler",
+            text: t("actions.cancel"),
             style: "cancel",
           },
           {
@@ -67,25 +104,26 @@ const Profile = () => {
       },
     },
     {
-      name: "Supprimer mon compte",
+      name: "profile.deletion.deleteAccount",
+      txtClassName: "text-red",
       onPress: () => {
         Alert.alert(
-          "Supprimer mon compte",
-          "Es-tu sûr de vouloir supprimer ton compte ?",
+          t("profile.deletion.deleteAccount"),
+          t("profile.deletion.areYouSure"),
           [
             {
-              text: "Annuler",
+              text: t("actions.cancel"),
               style: "cancel",
             },
             {
-              text: "Supprimer",
+              text: t("actions.delete"),
               onPress: () => {
                 deleteAuthUser()
                   .then(() => {
                     AsyncStorage.clear();
                     Alert.alert(
-                      "Compte supprimé",
-                      "Ton compte a bien été supprimé.",
+                      t("profile.deletion.accountDeleted"),
+                      t("profile.deletion.accountDeletedDescription"),
                       [
                         {
                           text: "OK",
@@ -108,33 +146,60 @@ const Profile = () => {
   return (
     <MyScreen className="px-0">
       <View className="px-4">
-        <MyHeader title={"Profil"} canGoBack />
+        <MyHeader canGoBack />
       </View>
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{
-          paddingTop: 20,
-          paddingHorizontal: 16,
+          paddingTop: 0,
+          paddingHorizontal: 32,
         }}
       >
-        {list.map((item, index) => (
-          <MyPressable // TODO change to DisplayInfo
-            key={index}
-            onPress={item.onPress}
-            className="items-center justify-between flex-row w-full rounded-2xl mb-3"
-          >
-            <MyText
-              className={`${item.name === "Supprimer mon compte" ? "text-red" : ""}`}
-            >
-              {item.name}
-            </MyText>
-            <MyText
-              className={`${item.name === "Supprimer mon compte" ? "text-red-300" : ""}`}
-            >
-              {"Bouyachaka"}
-            </MyText>
-          </MyPressable>
-        ))}
+        <View className="flex space-y-4">
+          <View className="relative flex items-center justify-center">
+            <View className="w-24 h-24 bg-pink-100 rounded-full overflow-hidden flex justify-center items-center">
+              <MyText className="text-5xl text-background-dark">L</MyText>
+            </View>
+
+            <View className="-translate-y-4">
+              <Bump scaleValue={0.95}>
+                <MyButton
+                  onPress={() => {}}
+                  txt="Add pictures"
+                  size="small"
+                  txtClassName="font-semibold"
+                />
+              </Bump>
+            </View>
+
+            <View className="flex items-center justify-center">
+              <Bump scaleValue={0.95}>
+                <MyButton
+                  onPress={() => {}}
+                  txt={t("profile.seeWhoLikedYou")}
+                  size="small"
+                  txtClassName="font-semibold text-gray-500"
+                  variant="pink"
+                  className="mb-2"
+                />
+              </Bump>
+              <MyText className="text-gray-400 font-thin">
+                Available in 7 circles
+              </MyText>
+            </View>
+          </View>
+
+          <View>
+            {list.map((item, index) => (
+              <DisplayInfo
+                key={index}
+                title={item.name}
+                value={item.value ?? ""}
+                txtClassName={item.txtClassName ?? ""}
+              />
+            ))}
+          </View>
+        </View>
       </ScrollView>
       <View className="px-4 self-start">
         <AppVersion />
