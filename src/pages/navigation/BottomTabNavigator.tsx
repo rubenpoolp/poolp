0a462c3064de4b2939e870e357fbfd8998a009ff
Enzo Ventura <enzo@trackap.com>
2024-10-15 20:22:14 +0200

    fix: avatar

--- src/pages/navigation/BottomTabNavigator.tsx
+++ src/pages/navigation/BottomTabNavigator.tsx
@@ -7,14 +7,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Camera, IntersectThree, UserFocus } from "phosphor-react-native";
 import React from "react";
 import { View } from "react-native";
+import Avatar from "@components/Avatar";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = [
+const Pages = [
   { name: "Home", component: Home, Icon: UserFocus },
   { name: "Circles", component: Circles, Icon: IntersectThree },
   { name: "Camera", component: CameraPage, Icon: Camera },
-  { name: "Profile", component: Profile, Icon: IntersectThree },
+  { name: "Profile", component: Profile, Icon: null },
 ];
 
 const BottomTabNavigator = () => {
@@ -28,12 +29,23 @@ const BottomTabNavigator = () => {
           paddingHorizontal: 40,
         },
         tabBarIcon: ({ focused }) => {
-          const tab = Tabs.find((t) => t.name === route.name);
+          const tab = Pages.find((t) => t.name === route.name);
           if (!tab) return null;
+
+          if (tab.name === "Profile") {
+            return (
+              <View className="items-center">
+                <Avatar size="sm" disabled={true} />
+                {focused && (
+                  <View className="absolute -bottom-[10px] w-[28px] h-[1px] bg-light" />
+                )}
+              </View>
+            );
+          }
 
           return (
             <View className="items-center">
-              <tab.Icon size={24} color={colors.light} />
+              {tab.Icon && <tab.Icon size={24} color={colors.light} />}
               {focused && (
                 <View className="absolute -bottom-[10px] w-[28px] h-[1px] bg-light" />
               )}
@@ -42,7 +54,7 @@ const BottomTabNavigator = () => {
         },
       })}
     >
-      {Tabs.map((tab) => (
+      {Pages.map((tab) => (
         <Tab.Screen key={tab.name} name={tab.name} component={tab.component} />
       ))}
     </Tab.Navigator>
