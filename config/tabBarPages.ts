b99acb43e156c00c3d7c162b21cd06d92e355d01
Enzo Ventura <enzo@trackap.com>
2024-10-16 18:08:30 +0200

    fix: pr corrections

---
+++ config/tabBarPages.ts
@@ -0,0 +1,12 @@
+import Circles from "@pages/circles/Circles";
+import Home from "@pages/home/Home";
+import Profile from "@pages/profile/Profile";
+import CameraPage from "@pages/takePicture/Camera";
+import { Camera, IntersectThree, UserFocus } from "phosphor-react-native";
+
+export const TabBarPages = [
+  { name: "Home", component: Home, Icon: UserFocus },
+  { name: "Circles", component: Circles, Icon: IntersectThree },
+  { name: "Camera", component: CameraPage, Icon: Camera },
+  { name: "Profile", component: Profile, Icon: null },
+  ];
