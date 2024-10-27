2cc7b8c98b5e72c7bf327536ff619a1d5f17d8db
aurelienv <aurelienvpro@gmail.com>
2024-10-27 12:49:29 +0100

    feat: format date for stories

--- src/hooks/useTodayCircle.ts
+++ src/hooks/useTodayCircle.ts
@@ -1,9 +1,10 @@
+import { UserStories } from "@/types/story";
 import useGetMyDailyCircle from "@api/circles/getMyDailyCircle.hook";
 import useGetAccounts from "@api/users/getAccounts.hook";
 import assets from "@assets/index";
 import { useAuth } from "@context/Auth";
-import { UserStories } from "@types/story";
-import { format } from "date-fns";
+import { formatStoryDate } from "@utils/dates";
+import { subDays, subHours } from "date-fns";
 
 const example_assets = [assets.test1, assets.test2];
 
@@ -15,8 +16,8 @@ const useTodayCircle = () => {
     const stories: UserStories = example_assets.map((pic, picIndex) => ({
       id: picIndex + index,
       picture: pic,
-      createdAt: format(new Date(), "LLL"),
-      updatedAt: format(new Date(), "LLL"),
+      createdAt: formatStoryDate(subDays(new Date(), 1)),
+      updatedAt: formatStoryDate(subHours(new Date(), 1)),
       userName: user?.name,
       userProfilePictureUrl:
         "https://dam.malt.com/0dd02c75-e1f6-4407-914b-acca8f88755d?gravity=face&func=face&face_margin=70&w=440&h=440&force_format=webp",
--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,10 +1,14 @@
-import { format } from "date-fns";
-import { fr } from "date-fns/locale";
+import { format, formatDistanceToNow } from "date-fns";
+import { enUS } from "date-fns/locale";
 
 export const readableDate = (date: Date) => {
-  return format(date, "dd MMMM yyyy", { locale: fr });
+  return format(date, "dd MMMM yyyy", { locale: enUS });
 };
 
 export const onlyHours = (date: Date) => {
-  return format(date, "HH:mm", { locale: fr });
+  return format(date, "HH:mm", { locale: enUS });
+};
+
+export const formatStoryDate = (date: Date) => {
+  return formatDistanceToNow(date, { locale: enUS });
 };
