2cfce80030215af79425e04f1ef51443db20b72b
aurelienv <aurelienvpro@gmail.com>
2024-11-13 12:13:31 +0100

    feat: change message for invite friends

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -2,7 +2,8 @@ import { Share } from "react-native";
 import i18n from "./i18n";
 
 export const shareToInviteFriends = () => {
-  const message = i18n.t("utils.messageInviteFriends");
+  const message = i18n.t("utils.messageInviteFriends") +
+    " https://www.poolp.app";
   Share.share({
     message,
   })
