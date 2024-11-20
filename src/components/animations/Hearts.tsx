33e78de25cea592943e214eadf525a117b8cb0a1
aurelienv <aurelienvpro@gmail.com>
2024-11-20 16:02:26 +0100

    fix: animation start bugged

--- src/components/animations/Hearts.tsx
+++ src/components/animations/Hearts.tsx
@@ -16,7 +16,7 @@ const Hearts = ({ isVisible }: { isVisible: boolean }) => {
   const hearts = Array(40)
     .fill(0)
     .map(() => ({
-      position: useRef(new Animated.Value(height - 120)).current,
+      position: useRef(new Animated.Value(height - 100)).current,
       xOffset: Math.random() * width,
       delay: Math.random() * 2000,
       duration: 3000 + Math.random() * 2000,
@@ -25,17 +25,20 @@ const Hearts = ({ isVisible }: { isVisible: boolean }) => {
   useEffect(() => {
     if (isVisible) {
       hearts.forEach((heart) => {
+        heart.position.setValue(height - 100);
+      });
+
+      hearts.forEach((heart) => {
         Animated.sequence([
           Animated.delay(heart.delay),
           Animated.timing(heart.position, {
             toValue: -150,
             duration: heart.duration,
             easing: Easing.out(Easing.bezierFn(0.79, 0.4, 0.83, 0.65)),
-
             useNativeDriver: true,
           }),
         ]).start(() => {
-          heart.position.setValue(height);
+          heart.position.setValue(height - 120);
         });
       });
     }
