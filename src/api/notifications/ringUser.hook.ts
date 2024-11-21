6601d77f4729bf4adf26968c0325496dedfb4fbd
Enzo Ventura <enzo@trackap.com>
2024-11-21 14:32:41 +0100

    feat: possibility to ring user every 5 minutes

--- src/api/notifications/ringUser.hook.ts
+++ src/api/notifications/ringUser.hook.ts
@@ -2,10 +2,11 @@ import { useMutation } from "@tanstack/react-query";
 import { myCaptureException } from "@utils/sentry";
 
 import getMyDailyCircleQuery from "@api/circles/getMyDailyCircle.query";
+import { ringUser } from "@api/notifications/ringUser.query";
+import { sendNotif } from "@api/notifications/sendNotif.query";
 import { useAuth } from "@context/Auth";
 import i18n from "@utils/i18n";
-import { ringUser } from "./ringUser.query";
-import { sendNotif } from "./sendNotif.query";
+import { Alert } from "react-native";
 
 const useRingUser = () => {
 
@@ -22,10 +23,19 @@ const useRingUser = () => {
         throw new Error("No circle found");
       }
 
-      ringUser(circle.id, auth.user?.id!, userId);
+      const success = await ringUser(circle.id, auth.user?.id!, userId);
+
+      if (!success) {
+        throw new Error("User already rung");
+      }
       return sendNotif([userId], i18n.t("notifications.ring.title"), i18n.t("notifications.ring.body"));
     },
     onError: (error: Error) => {
+      if (error.message === "User already rung") {
+        Alert.alert(i18n.t("notifications.ringExcessive.title"), i18n.t("notifications.ringExcessive.body"));
+        return;
+      }
+
       myCaptureException(error);
       throw new Error("Error sending notification", { cause: error });
     },
--- src/api/notifications/ringUser.query.ts
+++ src/api/notifications/ringUser.query.ts
@@ -2,19 +2,48 @@ import { Rings } from "@supabase_types";
 import { myCaptureException } from "@utils/sentry";
 import { supabase } from "@utils/supabase";
 
-export const ringUser = async (circleId: string, ringByUserId: string, userId: string) => {
+export const ringUser = async (circleId: string, ringByUserId: string, userId: string): Promise<boolean> => {
   const insertData: Partial<Rings> = {
     circle_id: circleId,
     ring_by_user_id: ringByUserId,
     user_id: userId,
   }
 
-  const {data, error} = await supabase.from("rings").insert(insertData);
+  const {data, error} = await supabase
+    .from("rings")
+    .select("created_at")
+    .eq("user_id", userId)
+    .eq("circle_id", circleId)
+    .eq("ring_by_user_id", ringByUserId)
+    .order("created_at", { ascending: false })
+    .limit(1);
 
   if (error) {
     myCaptureException(error);
-    throw new Error("Error ringing user", { cause: error });
+    throw new Error("Error getting last ring", { cause: error });
   }
 
-  return data;
+  if (data && data.length > 0) {
+    if (!data?.[0]?.created_at)
+      return false;
+    
+    const lastRingDate = new Date(data[0].created_at);
+    const currentDate = new Date();
+    const diffInMinutes = (currentDate.getTime() - lastRingDate.getTime()) / (1000 * 60);
+    
+    if (diffInMinutes < 5)
+      return false;
+  }
+
+  
+  const { data: _, error: insertError } = await supabase
+    .from("rings")
+    .insert(insertData);
+
+  if (insertError) {
+    myCaptureException(insertError);
+    throw new Error("Error ringing user", { cause: insertError });
+  }
+
+  return true;
 }
