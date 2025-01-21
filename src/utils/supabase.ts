9ee6257b7fc94306adfdd23c16f4900a03cbb4ed
aurelienv <aurelienvpro@gmail.com>
2025-01-21 23:36:56 +0100

    fix: try to fix auth disconection

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -20,10 +20,11 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 // to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
 // `SIGNED_OUT` event if the user's session is terminated. This should
 // only be registered once.
-AppState.addEventListener("change", (state) => {
-  if (state === "active") {
-    supabase.auth.startAutoRefresh();
-  } else {
-    supabase.auth.stopAutoRefresh();
-  }
+AppState.addEventListener("change", () => {
+  // if (state === "active") {
+  supabase.auth.startAutoRefresh();
+  // }
+  // else {
+  //   supabase.auth.stopAutoRefresh();
+  // }
 });
