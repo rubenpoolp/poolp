45e5445c7b393eb1d528b437309548a51fd6caa3
aurelienv <aurelienvpro@gmail.com>
2025-01-22 14:54:29 +0100

    fix: disconection new try / update i18n languages

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -90,7 +90,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     user: getAccount.data,
     isAdmin: getAccount.data?.role === "admin",
     session,
-    isLoading: getAccount.isFetching,
+    isLoading: getAccount.isLoading || getAccount.isFetching,
     sendSMS,
     checkCode,
     signUp,
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -20,11 +20,10 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 // to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
 // `SIGNED_OUT` event if the user's session is terminated. This should
 // only be registered once.
-AppState.addEventListener("change", () => {
-  // if (state === "active") {
-  supabase.auth.startAutoRefresh();
-  // }
-  // else {
-  //   supabase.auth.stopAutoRefresh();
-  // }
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
 });
